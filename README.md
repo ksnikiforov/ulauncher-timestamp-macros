2716dc234fc83a808ed9454e638de920f89637de
norter <kirnik77@gmail.com>
2021-03-03 19:25:44 +0300

    now only displays time

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Timestamp Macros Ulauncher Extension
+# Shows time in ulauncher
 
 ## Screenshot
 ![Alt text](images/screenshot.png)
--- main.py
+++ main.py
@@ -23,47 +23,11 @@ class KeywordQueryEventListener(EventListener):
         items = []
         logger.info('preferences %s' % json.dumps(extension.preferences))
 
-        print('{0:%Y-%m-%d}'.format(datetime.datetime.now()))
         items.append(ExtensionResultItem(icon='images/icon.png',
-                                         name='YYYY-MM-DD',
-                                         description='{0:%Y-%m-%d}'.format(datetime.datetime.now()),
-                                         on_enter=CopyToClipboardAction('{0:%Y-%m-%d}'.format(datetime.datetime.now()))))
-
-        items.append(ExtensionResultItem(icon='images/icon.png',
-                                         name='HH:mm',
-                                         description='{0:%H:%M}'.format(datetime.datetime.now()),
-                                         on_enter=CopyToClipboardAction(
-                                             '{0:%H:%M}'.format(datetime.datetime.now()))))
-
-        items.append(ExtensionResultItem(icon='images/icon.png',
-                                         name='YYYY-MM-DD HH:mm',
-                                         description='{0:%Y-%m-%d %H:%M}'.format(datetime.datetime.now()),
+                                         name='{0:%Y-%m-%d %H:%M}'.format(datetime.datetime.now()),
+                                         description=''),
                                          on_enter=CopyToClipboardAction(
                                              '{0:%Y-%m-%d %H:%M}'.format(datetime.datetime.now()))))
-
-        now = datetime.datetime.now()
-        time_intervals = [
-            (0, 3), (3, 6), (6, 9), (9, 12), (12, 15), (15, 18), (18, 21), (21,)
-        ]
-        current_time_interval = ''
-        for i in time_intervals:
-            if i[0] == 21:
-                current_time_interval = '21-24'
-                break
-            if i[0] <= now.hour and now.hour < i[1]:
-                current_time_interval = '{}-{}: '.format(i[0], i[1])
-                break
-        items.append(ExtensionResultItem(icon='images/icon.png',
-                                         name='HH1-HH2',
-                                         description=current_time_interval,
-                                         on_enter=CopyToClipboardAction(
-                                             current_time_interval)))
-
-        items.append(ExtensionResultItem(icon='images/icon.png',
-                                         name='HH1-HH2: HH:mm',
-                                         description='%s: %s' % (current_time_interval,'{0:%H:%M}'.format(datetime.datetime.now())),
-                                         on_enter=CopyToClipboardAction(
-                                             '%s: %s' % (current_time_interval,'{0:%H:%M}'.format(datetime.datetime.now())))))
         
         
         return RenderResultListAction(items)
